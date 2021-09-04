d9e1c53e43dfb6390442feb9e09e41333d6e3067
pot-code <136290354@qq.com>
2021-09-04 23:12:53 +0800

    feat: create ExitManager

--- pkg/util/hooks.go
+++ pkg/util/hooks.go
@@ -7,10 +7,27 @@ import (
 	"time"
 )
 
-func OnExit(timeout time.Duration, cb func(ctx context.Context)) {
+type ExitHandler func(ctx context.Context)
+
+type ExitManager struct {
+	handlers []ExitHandler
+}
+
+func NewExitManager() *ExitManager {
+	return &ExitManager{
+		handlers: []ExitHandler{},
+	}
+}
+
+func (em ExitManager) Register(handler ExitHandler) {
+	em.handlers = append(em.handlers, handler)
+}
+
+func (em ExitManager) Wait(timeout time.Duration) {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 	<-ch
+
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	go func() {
@@ -18,5 +35,8 @@ func OnExit(timeout time.Duration, cb func(ctx context.Context)) {
 		cancel()
 		os.Exit(1)
 	}()
-	cb(ctx)
+
+	for _, h := range em.handlers {
+		h(ctx)
+	}
 }
