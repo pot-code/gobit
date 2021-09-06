21b09c8f401d31260f1ae9e6a9200ee17f81ae07
pot-code <136290354@qq.com>
2021-09-06 10:13:59 +0800

    refactor(logging): remove creation

--- pkg/logging/zap.go
+++ pkg/logging/zap.go
@@ -5,92 +5,51 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
+	"github.com/pkg/errors"
 	gobit "github.com/pot-code/gobit/pkg"
+	"github.com/pot-code/gobit/pkg/util"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var DefaultLoggingContextKey = gobit.AppContextKey("logger")
 
-// LoggerConfig options used in creating zap logger
-type LoggerConfig struct {
-	FilePath  string // log file path
-	Level     string // global logging level
-	AddCaller bool
+func NewEcsConfig() zapcore.EncoderConfig {
+	ec := zap.NewProductionEncoderConfig()
+
+	ec.EncodeTime = zapcore.TimeEncoder(zapcore.ISO8601TimeEncoder)
+	ec.TimeKey = "@timestamp"
+	ec.MessageKey = "message"
+	ec.LevelKey = "log.level"
+	ec.CallerKey = "log.origin.file.line"
+	ec.StacktraceKey = "error.stack_trace"
+	return ec
 }
 
-// NewLogger returns a zap logger
-func NewLogger(cfg *LoggerConfig) (*zap.Logger, error) {
-	var (
-		core zapcore.Core
-		err  error
-	)
-	core, err = NewProductionCore(cfg)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create logger core::%w", err)
-	}
-
-	logger := zap.New(core)
-	if cfg.AddCaller {
-		logger.WithOptions(zap.AddCaller())
-	}
-	return logger, nil
-}
-
-func NewProductionCore(cfg *LoggerConfig) (zapcore.Core, error) {
-	logEnabler, err := getLevelEnabler(cfg.Level)
-	if err != nil {
-		return nil, err
-	}
-
-	ecsEncoderConfig := zap.NewProductionEncoderConfig()
-	ecsEncoderConfig.EncodeTime = zapcore.TimeEncoder(func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-		enc.AppendString(t.UTC().Format("2006-01-02T15:04:05.000Z"))
-	})
-	ecsEncoderConfig.TimeKey = "@timestamp"
-	ecsEncoderConfig.MessageKey = "message"
-	ecsEncoderConfig.LevelKey = "log.level"
-	ecsEncoderConfig.CallerKey = "log.position"
-	ecsEncoderConfig.StacktraceKey = "error.stack_trace"
-	ecsEncoder := zapcore.NewJSONEncoder(ecsEncoderConfig)
-
-	if cfg.FilePath != "" {
-		elkOutput, err := getFileSyncer(cfg.FilePath)
-		return zapcore.NewCore(ecsEncoder, elkOutput, logEnabler), err
-	}
-	return zapcore.NewCore(ecsEncoder, os.Stderr, logEnabler), nil
-}
-
-func getFileSyncer(p string) (zapcore.WriteSyncer, error) {
-	fd, err := os.OpenFile(p, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+func NewFileSyncer(p string) (zapcore.WriteSyncer, error) {
+	fd, err := os.OpenFile(p, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
 		return nil, err
 	}
 	return fd, err
 }
 
-func getZapLoggingLevel(l string) (level zapcore.Level) {
-	switch strings.ToLower(l) {
-	case "debug":
-		level = zap.DebugLevel
-	case "info":
-		level = zap.InfoLevel
-	case "warn":
-		level = zap.WarnLevel
-	case "error":
-		level = zap.ErrorLevel
-	case "fatal":
-		level = zap.FatalLevel
-	default:
-		level = zap.InfoLevel
-	}
-	return
+var zapLoggingLevelMap map[string]zapcore.Level = map[string]zapcore.Level{
+	"debug":  zap.DebugLevel,
+	"info":   zap.InfoLevel,
+	"warn":   zap.WarnLevel,
+	"error":  zap.ErrorLevel,
+	"fatal":  zap.FatalLevel,
+	"panic":  zap.PanicLevel,
+	"dpanic": zap.DPanicLevel,
 }
 
-func getLevelEnabler(l string) (zapcore.LevelEnabler, error) {
-	level := getZapLoggingLevel(l)
+func GetLevelEnabler(l string) (zapcore.LevelEnabler, error) {
+	level, ok := zapLoggingLevelMap[strings.ToLower(l)]
+	if !ok {
+		return nil, fmt.Errorf("unsupported logging level '%s'", l)
+	}
 	return zap.LevelEnablerFunc(func(lv zapcore.Level) bool {
 		return lv >= level
 	}), nil
@@ -104,4 +63,43 @@ func InjectContext(ctx context.Context, logger *zap.Logger) context.Context {
 // ExtractFromContext try to extract logger from context
 func ExtractFromContext(ctx context.Context) *zap.Logger {
 	return ctx.Value(DefaultLoggingContextKey).(*zap.Logger)
+}
+
+type ZapErrorWrapper struct {
+	depth int
+	err   error
+}
+
+// NewZapErrorWrapper create wrapper object that implements the `MarshalLogObject` protocol
+//
+// depth: set stack trace depth if the error type supports it
+//
+// err: the error to be wrapped
+func NewZapErrorWrapper(err error, depth int) *ZapErrorWrapper {
+	return &ZapErrorWrapper{depth, err}
+}
+
+func (te ZapErrorWrapper) Unwrap() error {
+	return te.err
+}
+
+func (te ZapErrorWrapper) Error() string {
+	if te.err != nil {
+		return te.err.Error()
+	}
+	return "traceable error"
+}
+
+func (te ZapErrorWrapper) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	err := te.err
+	if ste, ok := err.(util.StackTracer); ok {
+		trace := util.GetVerboseStackTrace(te.depth, ste)
+		enc.AddString("stack_trace", trace)
+
+		cause := errors.Cause(err)
+		enc.AddString("message", cause.Error())
+	} else {
+		enc.AddString("message", err.Error())
+	}
+	return nil
 }
