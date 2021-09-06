acaf9bbc7be0012106e9375bdd26913ac5431f5b
pot-code <136290354@qq.com>
2021-09-06 14:06:09 +0800

    refactor(util): create ConfigManager

--- pkg/util/config.go
+++ pkg/util/config.go
@@ -2,86 +2,99 @@ package util
 
 import (
 	"errors"
-	"fmt"
-	"log"
-	"reflect"
 	"strings"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
-// LoadConfig init app config using viper
-func LoadConfig(envPrefix string, o interface{}) error {
-	pflag.Parse()
-	viper.BindPFlags(pflag.CommandLine)
+type ConfigOption interface {
+	apply(*ConfigManager)
+}
+
+type optionFunc func(*ConfigManager)
+
+func (f optionFunc) apply(cm *ConfigManager) {
+	f(cm)
+}
+
+func AddConfigPath(p string) ConfigOption {
+	return optionFunc(func(cm *ConfigManager) {
+		cm.configPaths[p] = struct{}{}
+	})
+}
+
+func WithConfigName(name string) ConfigOption {
+	return optionFunc(func(cm *ConfigManager) {
+		cm.configName = name
+	})
+}
+
+func WithConfigType(t string) ConfigOption {
+	return optionFunc(func(cm *ConfigManager) {
+		cm.configType = t
+	})
+}
+
+func WithEnvPrefix(prefix string) ConfigOption {
+	return optionFunc(func(cm *ConfigManager) {
+		cm.envPrefix = prefix
+	})
+}
+
+var (
+	ErrNoConfigFileFound = errors.New("no config file found")
+)
+
+type ConfigManager struct {
+	configName  string
+	configType  string
+	configPaths map[string]struct{}
+	envPrefix   string
+}
+
+func NewConfigManager(options ...ConfigOption) *ConfigManager {
+	cm := &ConfigManager{
+		envPrefix:  "GO",
+		configName: "config",
+		configType: "yml,",
+		configPaths: map[string]struct{}{
+			".": {},
+		},
+	}
+
+	for _, option := range options {
+		option.apply(cm)
+	}
 
 	// env
 	viper.AutomaticEnv()
-	viper.SetEnvPrefix(envPrefix)
+	viper.SetEnvPrefix(cm.envPrefix)
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	// config files
-	viper.SetConfigName("config")   // name of config file (without extension)
-	viper.SetConfigType("yaml")     // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("/etc/go/") // path to look for the config file in
-	viper.AddConfigPath("$HOME/")   // call multiple times to add many search paths
-	viper.AddConfigPath(".")        // optionally look for config in the working directory
+	viper.SetConfigName(cm.configName) // name of config file (without extension)
+	for p := range cm.configPaths {
+		viper.AddConfigPath(p)
+	}
+	return cm
+}
+
+// LoadConfig load config into cfg
+func (cm *ConfigManager) LoadConfig(cfg interface{}) error {
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Printf("[skipped]config file dose not present.")
+			return ErrNoConfigFileFound
 		} else {
 			return err
 		}
 	}
 
-	if err := viper.Unmarshal(o); err != nil {
-		return err
-	}
-
-	if err := validateConfig(o); err != nil {
+	if err := viper.Unmarshal(cfg); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func validateConfig(config interface{}) error {
-	validate := validator.New()
-
-	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
-		name := fld.Tag.Get("json")
-		if name == "-" || name == "" {
-			name = fld.Tag.Get("yaml")
-			if name == "-" || name == "" {
-				return ""
-			}
-		}
-		return name
-	})
-
-	err := validate.Struct(config)
-	if err == nil {
-		return nil
-	} else if errors.Is(err, &validator.InvalidValidationError{}) {
-		return fmt.Errorf("failed to validate config: %s", err)
-	}
-
-	var msg []string
-	for _, field := range err.(validator.ValidationErrors) {
-		fieldName := field.Namespace()
-		switch field.Tag() {
-		case "required":
-			msg = append(msg, fmt.Sprintf("%s is required", fieldName))
-		case "oneof":
-			msg = append(msg, fmt.Sprintf("%s must be one of (%s)", fieldName, field.Param()))
-		default:
-			msg = append(msg, field.Error())
-		}
-	}
-	if len(msg) > 0 {
-		return fmt.Errorf("failed to validate config: \n%s", strings.Join(msg, "\n"))
-	}
-	return err
 }
