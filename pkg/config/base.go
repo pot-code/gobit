176ebb46dd990d60e73ba5e5a502e4c7dd118226
pot-code <136290354@qq.com>
2021-09-16 21:46:57 +0800

    fix: remove port from BaseConfig

--- pkg/config/base.go
+++ pkg/config/base.go
@@ -2,6 +2,5 @@ package config
 
 type BaseConfig struct {
 	AppID string `validate:"required" mapstructure:"app_id" yaml:"app_id"`               // application name
-	Port  string `mapstructure:"port" yaml:"port"`                                       // bind listen port
 	Env   string `validate:"oneof=development production" mapstructure:"env" yaml:"env"` // runtime environment
 }
